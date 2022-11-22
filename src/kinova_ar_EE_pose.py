61580a558c3b6e70e6c1a6f96ab30246cd2b4423
DreVinciCode <andre.cleaver@tufts.edu>
2022-11-22 16:11:24 -0500

    Update kinova_ar_EE_pose.py

--- src/kinova_ar_EE_pose.py
+++ src/kinova_ar_EE_pose.py
@@ -10,27 +10,39 @@ class messageConverter:
     def __init__(self):
     	rospy.init_node('PoseToPoint', anonymous = True)
     	self.base_feedback_sub = rospy.Subscriber("/my_gen3_lite/base_feedback", BaseCyclic_Feedback, self.base_feedback_callback)
-    	self.Twist_pub = rospy.Publisher("/my_gen3_lite/end_effector_pose", Twist, queue_size=1)
+    	self.Kinova_Position = rospy.Publisher("/my_gen3_lite/end_effector_pose", Twist, queue_size=1)
+        self.Kinova_Velocity = rospy.Publisher("KinovaAR/end_effector_velocity", Twist, queue_size=1)
     	rospy.spin()
 
     def base_feedback_callback(self, data):
-    	linearX = data.base.commanded_tool_pose_x
-        linearY = data.base.commanded_tool_pose_y
-        linearZ = data.base.commanded_tool_pose_z
+    	position_x = data.base.commanded_tool_pose_x
+        position_y = data.base.commanded_tool_pose_y
+        position_z = data.base.commanded_tool_pose_z
 
         angularX = data.base.commanded_tool_pose_theta_x
         angularY = data.base.commanded_tool_pose_theta_y
         angularZ = data.base.commanded_tool_pose_theta_z
 
-    	Twist_Message = Twist()
-    	Twist_Message.linear.x = linearX
-    	Twist_Message.linear.y = linearY
-    	Twist_Message.linear.z = linearZ
-        Twist_Message.angular.x = angularX
-        Twist_Message.angular.y = angularY
-        Twist_Message.angular.z = angularZ
+        velocity_x = data.base.tool_twist_linear_x
+        velocity_y = data.base.tool_twist_linear_y
+        velocity_z = data.base.tool_twist_linear_z
 
-    	self.Twist_pub.publish(Twist_Message)
+        Twist_Message = Twist()
+        Twist_Message.linear.x = velocity_x
+        Twist_Message.linear.y = velocity_y
+        Twist_Message.linear.z = velocity_z
+
+        self.Kinova_Velocity.publish(Twist_Message)
+
+    	Position_Message = Twist()
+    	Position_Message.linear.x = position_x
+    	Position_Message.linear.y = position_y
+    	Position_Message.linear.z = position_z
+        Position_Message.angular.x = angularX
+        Position_Message.angular.y = angularY
+        Position_Message.angular.z = angularZ
+
+    	self.Kinova_Position.publish(Position_Message)
 
 if __name__ == '__main__':
     converter = messageConverter()
