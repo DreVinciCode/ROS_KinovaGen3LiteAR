7062a12de896e67c8659a976b31a4ffb33bcf5bd
DreVinciCode <andre.cleaver@tufts.edu>
2023-11-29 16:37:49 -0500

    Update RecordKeyPress.py

--- src/RecordKeyPress.py
+++ src/RecordKeyPress.py
@@ -6,10 +6,15 @@ from std_msgs.msg import *
 
 rospy.init_node('RecordKeypress')
 Empty_Publisher = rospy.Publisher("/KinovaAR/save", Empty, queue_size=1)
+DRT_Publisher = rospy.Publisher("/KinovaAR/DRT_return", Empty, queue_size=1)
 
 record_check = True
 
 while True:
+    if keyboard.is_pressed('space'):
+        print("Spaced Pressed!")
+        DRT_Publisher.publish(Empty())
+
     if keyboard.read_key() == 'n':
         record_check = True
 
