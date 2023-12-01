5b3bf4297bb0d0b9e0bc6380173468337c0ad332
DreVinciCode <andre.cleaver@tufts.edu>
2023-12-01 16:46:20 -0500

    Update DRT_Manager.py

--- src/DRT_Manager.py
+++ src/DRT_Manager.py
@@ -1,63 +1,79 @@
 #!/usr/bin/env python3
 
-import keyboard
 import rospy
 import threading
 import random
-from std_msgs.msg import Empty, Float32
+from std_msgs.msg import Empty
 import time
+from kinova_ar.srv import SendDRTScore, SendDRTScoreResponse
+from std_srvs.srv import Empty as EmptyService
 
 class DRT_Manager(object):
     def __init__(self):
 
         rospy.init_node('drt_manager', anonymous=True)
+
         self.pub = rospy.Publisher('/KinovaAR/DRT_signal', Empty, queue_size=1)
-        self.sub = rospy.Subscriber('/KinovaAR/DRT_response', Empty, self.response_callback)
-        
-        self.accuracy_pub = rospy.Publisher("/KinovaAR/DRT_accuracy", Float32, queue_size=1)
 
-        self.response_received = False
+        rospy.Service('/KinovaAR/start_drt', EmptyService, self.start_drt)
+        rospy.Service('/KinovaAR/stop_drt', SendDRTScore, self.send_score)
 
-        self.key_pressed = False
-        self.counter = 0
+        self.run_drt = None
+        self.counter = None
+        self.success_counter = None
+        self.signal_thread = None
+
+        self.start_drt()
+
+    def start_drt(self) -> threading.Thread:
         self.success_counter = 0
-        self.next_publish_time = rospy.Time.now()
+        self.counter = 0
 
-        signal_thread = threading.Thread(target=self.drt_signal_thread)
-        signal_thread.daemon = True
-        signal_thread.start()
-        # while not rospy.is_shutdown():
-        #     self.drt_signal_thread()
+        self.run_drt = True
 
-    def response_callback(self, data):
-        current_time = rospy.Time.now()
-        if (self.next_publish_time - current_time) >= rospy.Duration(2.5):
-            rospy.loginfo("Response received within 2500ms after publishing")
-            self.success_counter += 1
-            # self.calculate_rate()
-        else:
-            rospy.loginfo("Response NOT received within 2500ms after publishing")
-        self.response_received = True
-
-    def calculate_rate(self):
-        accuracy = self.success_counter / self.counter
-        self.accuracy_pub.publish(accuracy)
-        print(accuracy) 
-
+        self.signal_thread = threading.Thread(target=self.drt_signal_thread)
+        self.signal_thread.daemon = True
+        self.signal_thread.start()
 
     def drt_signal_thread(self):
-        while not rospy.is_shutdown():
-            current_time = rospy.Time.now()
-            if current_time >= self.next_publish_time and not self.response_received:
-                rand = random.uniform(3, 5)
-                self.next_publish_time = current_time + rospy.Duration(rand)
-                self.pub.publish(Empty())
-                self.counter += 1
-                print("Published message. Total count: %d", self.counter)
+        time_sleep = random.uniform(2.5, 5)
+        while self.run_drt:
+            time.sleep(time_sleep)
+            self.pub.publish(Empty())
+            self.counter += 1
+            rospy.loginfo("Published message. Total count: %d", self.counter)
 
-            self.response_received = False
-            time.sleep(0.1)  # Adjust the sleep time as needed
+            try:
+                send_time = rospy.Time.now()
 
+                rospy.wait_for_message('/KinovaAR/DRT_response', Empty, timeout=rospy.Duration(2.5))
+                time_taken = (rospy.Time.now() - send_time).to_sec()
+
+                rospy.loginfo("Recieved response from participant")
+                self.success_counter += 1
+                time_sleep = random.uniform(3 - time_taken, 5 - time_taken)
+            except rospy.ROSException:
+                rospy.logerr("No response recieved...")
+                time_sleep = random.uniform(0.5, 2.5)
+
+            rospy.loginfo(f"Current score: {self.success_counter / self.counter}")
+
+    def send_score(self, data: SendDRTScore) -> SendDRTScoreResponse:
+        '''return drt accuracy score to service sender
+        '''
+        self.run_drt = False
+
+        if self.signal_thread is None:
+            rospy.logerr("No thread is running....")
+            return
+
+        self.signal_thread.join()
+        self.signal_thread = None
+
+        score = self.success_counter / self.counter
+        rospy.loginfo(f"Final score: {score}")
+
+        return SendDRTScoreResponse(score)
 
 
 if __name__ == '__main__':
